9668d33df6130b2f616b8c80cd6dcfc758539858
Mikhail Biltsevich <mikebiltsevich@gmail.com>
2021-02-04 15:20:00 +0300

    feat: add input error

--- src/components/InputComponent/InputComponent.tsx
+++ src/components/InputComponent/InputComponent.tsx
@@ -5,20 +5,40 @@ interface IProps {
   label: string
   type: string
   handler: (event: any) => void
+  error?: string
 }
 
 class InputComponent extends React.Component<IProps> {
   render() {
     return (
-      <div className='input-group vertical'>
-        <label htmlFor={this.props.id}>
-          {this.props.label}
-        </label>
-        <input
-          type={this.props.type}
-          id={this.props.id}
-          placeholder={this.props.label}
-          onChange={this.props.handler} />
+      <div className="container">
+        <div className='input-group vertical'>
+          <label htmlFor={this.props.id}>
+            {this.props.label}
+          </label>
+
+          {this.props.error ?
+            <input style={{ borderColor: '#d32f2f' }}
+              type={this.props.type}
+              id={this.props.id}
+              placeholder={this.props.label}
+              onChange={this.props.handler} />
+            :
+            <input
+              type={this.props.type}
+              id={this.props.id}
+              placeholder={this.props.label}
+              onChange={this.props.handler} />
+          }
+
+        </div>
+
+        {this.props.error &&
+          <div className='card error'>
+            <h3>Error</h3>
+            <p>{this.props.error}</p>
+          </div>
+        }
       </div>
     );
   }
