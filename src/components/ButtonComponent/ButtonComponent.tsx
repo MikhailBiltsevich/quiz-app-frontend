667823d33dfd41b3cecf8654cde6f64b1a9267c9
Mikhail Biltsevich <mikebiltsevich@gmail.com>
2021-02-04 16:21:11 +0300

    feat: add classNames property

--- src/components/ButtonComponent/ButtonComponent.tsx
+++ src/components/ButtonComponent/ButtonComponent.tsx
@@ -3,10 +3,13 @@ import { SyntheticEvent } from 'react';
 interface IProps {
   text: string
   handlerClick: (event: SyntheticEvent) => void
+  classNames?: string[]
 }
 
 export default function ButtonComponent(props: IProps) {
   return (
-    <button onClick={props.handlerClick}>{props.text}</button>
+    props.classNames
+      ? <button className={props.classNames.join(' ')} onClick={props.handlerClick}>{props.text}</button>
+      : <button onClick={props.handlerClick}>{props.text}</button>
   );
 }
