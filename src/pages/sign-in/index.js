ba1cdb69cf9c57c3eec227b8162699e1b36cf210
Mikhail Biltsevich <mikebiltsevich@gmail.com>
2021-02-08 17:01:42 +0300

    feat: implement form in sign-in-page

---
+++ src/pages/sign-in/index.js
@@ -0,0 +1,14 @@
+import ButtonComponent from "../../components/ButtonComponent/ButtonComponent";
+import InputComponent from "../../components/InputComponent/InputComponent";
+
+export default function SignInPage(props) {
+  return (
+    <div className='row'>
+      <form className='col-sm-10 col-md-3'>
+        <InputComponent id='login' label='Username' type='text' handler={() => {}} />
+        <InputComponent id='password' label='Password' type='password' handler={() => {}} />
+        <ButtonComponent text='Sign in' classNames={['primary']} />
+      </form>
+    </div>
+  );
+}
