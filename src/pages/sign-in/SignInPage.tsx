4af456dda552238afdde4aeb5e118521efddf90e
Mikhail Biltsevich <mikebiltsevich@gmail.com>
2021-02-09 16:51:16 +0300

    fix: ButtonComponent's props (add handler)

--- src/pages/sign-in/SignInPage.tsx
+++ src/pages/sign-in/SignInPage.tsx
@@ -1,13 +1,13 @@
 import ButtonComponent from "../../components/ButtonComponent/ButtonComponent";
 import InputComponent from "../../components/InputComponent/InputComponent";
 
- const SignInPage = () => {
+const SignInPage = () => {
   return (
     <div className='row'>
       <form className='col-sm-10 col-md-3'>
         <InputComponent id='login' label='Username' type='text' handler={() => { }} />
         <InputComponent id='password' label='Password' type='password' handler={() => { }} />
-        <ButtonComponent text='Sign in' classNames={['primary']}/>
+        <ButtonComponent text='Sign in' classNames={['primary']} handlerClick={() => { }} />
       </form>
     </div>
   );
