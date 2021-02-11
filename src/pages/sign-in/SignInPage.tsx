acd60390284914993c2e9debfe5e871b6e9899ec
Mikhail Biltsevich <mikebiltsevich@gmail.com>
2021-02-11 21:10:43 +0300

    feat: implement handlers skeleton

--- src/pages/sign-in/SignInPage.tsx
+++ src/pages/sign-in/SignInPage.tsx
@@ -2,12 +2,16 @@ import ButtonComponent from "../../components/ButtonComponent/ButtonComponent";
 import InputComponent from "../../components/InputComponent/InputComponent";
 
 const SignInPage = () => {
+  const handleSubmit = () => { };
+  const handlePasswordChange = () => { };
+  const handleUsernameChange = () => { };
+
   return (
     <div className='row'>
       <form className='col-sm-10 col-md-3'>
-        <InputComponent id='login' label='Username' type='text' handler={() => { }} />
-        <InputComponent id='password' label='Password' type='password' handler={() => { }} />
-        <ButtonComponent text='Sign in' classNames={['primary']} handlerClick={() => { }} />
+        <InputComponent id='login' label='Username' type='text' onChange={handleUsernameChange} />
+        <InputComponent id='password' label='Password' type='password' onChange={handlePasswordChange} />
+        <ButtonComponent text='Sign in' classNames={['primary']} onClick={handleSubmit} />
       </form>
     </div>
   );
