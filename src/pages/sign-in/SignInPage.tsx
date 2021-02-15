8347b43fd8a2b0bf91d948e5164c6d6494fab01a
Mikhail Biltsevich <mikebiltsevich@gmail.com>
2021-02-15 13:15:22 +0300

    fix: change responsive container for <form>

--- src/pages/sign-in/SignInPage.tsx
+++ src/pages/sign-in/SignInPage.tsx
@@ -8,11 +8,13 @@ const SignInPage = () => {
 
   return (
     <div className='row'>
-      <form className='col-sm-10 col-md-3'>
-        <InputComponent id='login' label='Username' type='text' onChange={handleUsernameChange} />
-        <InputComponent id='password' label='Password' type='password' onChange={handlePasswordChange} />
-        <ButtonComponent text='Sign in' classNames={['primary']} onClick={handleSubmit} />
-      </form>
+      <div className='col-sm-12 col-md-5 col-lg-3'>
+        <form>
+          <InputComponent id='login' label='Username' type='text' onChange={handleUsernameChange} />
+          <InputComponent id='password' label='Password' type='password' onChange={handlePasswordChange} />
+          <ButtonComponent text='Sign in' classNames={['primary']} onClick={handleSubmit} />
+        </form>
+      </div>
     </div>
   );
 }
