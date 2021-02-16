be45427bc96aaec811f506118914412bd299f5dd
Mikhail Biltsevich <mikebiltsevich@gmail.com>
2021-02-16 15:58:27 +0300

    feat: add content wrappers

--- src/pages/quiz/QuizPage.tsx
+++ src/pages/quiz/QuizPage.tsx
@@ -1,5 +1,21 @@
+import CardComponent from "../../components/CardComponent/CardComponent"
+
 const QuizPage = () => {
-  return <></>;
+  return (
+    <div className='container'>
+      <div className="row">
+        <div className="col-sm-2">
+          <CardComponent classNames={['fluid']} >
+
+          </CardComponent>
+        </div>
+        <div className="col-sm-8 col-sm-offset-1">
+
+        </div>
+      </div>
+    </div>
+
+  );
 }
 
 export default QuizPage;
