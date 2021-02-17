78e10a6cb76bd59d2253bda07423981c08e4e5db
Mikhail Biltsevich <mikebiltsevich@gmail.com>
2021-02-17 17:39:51 +0300

    feat: implement quiz form

--- src/pages/quiz/QuizPage.tsx
+++ src/pages/quiz/QuizPage.tsx
@@ -1,4 +1,8 @@
-import CardComponent from "../../components/CardComponent/CardComponent"
+import ButtonComponent from "../../components/ButtonComponent/ButtonComponent";
+import CardComponent from "../../components/CardComponent/CardComponent";
+import InputComponent from "../../components/InputComponent/InputComponent";
+import SelectComponent from "../../components/SelectComponent/SelectComponent";
+import CollapseSectionComponent from "../../components/CollapseSectionComponent/CollapseSectionComponent";
 
 interface IProps {
   quiz: {
@@ -13,20 +17,61 @@ interface IProps {
 
 const QuizPage = (props: IProps) => {
   const { title, questions } = props.quiz;
+  const questionType = [
+    { key: 'single', value: 'Single' },
+    { key: 'multiply', value: 'Multiply' }
+  ];
+
+  const templateHandler = () => { };
 
   return (
     <div className='container'>
       <div className="row">
-        <div className="col-sm-2">
-          <CardComponent classNames={['fluid']} >
+        <div className="col-md-5">
+          <form>
+            <fieldset>
+              <legend>Quiz</legend>
+              <InputComponent label='Title' onChange={templateHandler} type='text' id='quiz-title-input'></InputComponent>
+            </fieldset>
+          </form>
 
-          </CardComponent>
-        </div>
-        <div className="col-sm-8 col-sm-offset-1">
+          <form>
+            <fieldset>
+              <legend>Question</legend>
+              <div className="row">
+                <div className="col-md">
+                  <InputComponent label='Text' onChange={templateHandler} type='text' id='question-text-input' />
+                  <div className='input-group vertical'>
+                    <label>Type</label>
+                    <SelectComponent options={questionType} onChange={templateHandler}></SelectComponent>
+                  </div>
+                </div>
+              </div>
 
+              <div className='input-group vertical'>
+                <label>Answers</label>
+                <SelectComponent options={[]} onChange={templateHandler}></SelectComponent>
+              </div>
+              <div className="button-group">
+                <ButtonComponent classNames={['primary']} text='Add' onClick={templateHandler}></ButtonComponent>
+                <ButtonComponent text='Reset' onClick={templateHandler}></ButtonComponent>
+              </div>
+            </fieldset>
+          </form>
+
+          <form>
+            <fieldset>
+              <legend>Answer</legend>
+              <InputComponent label='Text' onChange={templateHandler} type='text' id='answer-text-input' />
+              <div className="button-group">
+                <ButtonComponent classNames={['primary']} text='Add' onClick={templateHandler}></ButtonComponent>
+                <ButtonComponent text='Reset' onClick={templateHandler}></ButtonComponent>
+              </div>
+            </fieldset>
+          </form>
         </div>
-      </div>
-    </div>
+      </div >
+    </div >
 
   );
 }
